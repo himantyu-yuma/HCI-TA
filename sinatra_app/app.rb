96a10d70eae4d22237a765d0c68331b5a6c1f0cd
Naoki <36870844+Naoki08@users.noreply.github.com>
2023-01-24 17:10:01 +0900

    基本課題

--- sinatra_app/app.rb
+++ sinatra_app/app.rb
@@ -1,42 +1,47 @@
 # coding: utf-8
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 
-
 ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3',
-    database: 'seiseki.db'
+    database: 'laboratory.db',
 )
 
-class StudentRecord < ActiveRecord::Base
+class Lab < ActiveRecord::Base
 end
 
-class Student < ActiveRecord::Base
+class LabMember < ActiveRecord::Base
 end
 
 get '/' do
-    @title = 'Hello, world!'
-    @world = 'world!'
     erb :index
 end
 
-get '/seiseki' do
-    @title = 'Seiseki View'
-    @students = StudentRecord.all
-    erb :seiseki
-end
-
-post '/judge' do
-    if !Student.where(:student_id => params[:student_id], :name => params[:name]).empty?
-        @title = 'View Seiseki'
-        @student = StudentRecord.where(:student_id => params[:student_id])
-        @student_name = params[:name]
-        erb :login
-    else
-        redirect '/'
+post '/lab_search' do
+    @title = 'Lab Search'
+    lab_name = params["lab_name"]
+    lab_id = Lab.where(:lab_name => lab_name)
+                .select(:id)
+    @members = LabMember.where(:lab_id => lab_id)
+                        .select(:member_name)
+    if @members.empty?
+        @error_message = "研究室名：#{lab_name}は存在しません．"
     end
+    erb :lab_search
 end
 
-
-# bundle exec ruby app.rb -o 0.0.0.0 のオプションでホストからも確認できる
+post '/member_search' do
+    @title = 'Member Search'
+    member_name = params["member_name"]
+    lab_id = LabMember.where(:member_name => member_name)
+                      .select(:lab_id)
+    @members = LabMember.where(:lab_id => lab_id)
+                        .where.not(:member_name => member_name)
+                        .select(:member_name)
+    if @members.empty?
+        @error_message = "メンバー名：#{member_name}は存在しません"
+    end
+    erb :member_search
+end
